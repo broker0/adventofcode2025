7fcf079abec38a6e312fead52b5eba1a4614ad84
Broker0 <brok79@internet.ru>
2025-12-07 18:04:04 +0500

    small improve

--- day07.py
+++ day07.py
@@ -1,23 +1,23 @@
-
+from collections import Counter
 
 with open('day07.txt', 'rt') as fl:
     s = fl.readline()
     split_count = 0
-    current_beams = {s.index('S'): 1}
-    splits = dict()
-    for y, line in enumerate(fl):
-        new_beams = dict()
+    current_beams = Counter({s.index('S'): 1})
+
+    for line in fl:
+        new_beams = Counter()
         line = line.strip()
         for (curr_beam_x, curr_beam_cnt) in current_beams.items():
             if line[curr_beam_x] == '.':
-                new_beams[curr_beam_x] = new_beams.get(curr_beam_x, 0) + curr_beam_cnt
+                new_beams[curr_beam_x] += curr_beam_cnt
 
             elif line[curr_beam_x] == '^':
-                new_beams[curr_beam_x-1] = new_beams.get(curr_beam_x-1, 0) + curr_beam_cnt
-                new_beams[curr_beam_x+1] = new_beams.get(curr_beam_x+1, 0) + curr_beam_cnt
+                new_beams[curr_beam_x-1] += curr_beam_cnt
+                new_beams[curr_beam_x+1] += curr_beam_cnt
                 split_count += 1
 
         current_beams = new_beams
 
     print(split_count)
-    print(sum(current_beams.values()))
+    print(current_beams.total())
